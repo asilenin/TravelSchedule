cad77eb5cf3509f62d6d3b431f3ce7e9431c9142
Anton Silenin <anton-silenin@yandex.ru>
2026-01-16 20:34:33 +0300

    chore: add Xcode project and .gitignore

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Anton Silenin on 16.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
