6dad758965bba79b6a0c9e2ef876a1decbdaa8f7
Anton Silenin <anton-silenin@yandex.ru>
2026-01-24 18:37:37 +0300

    feat: add API key configuration and test API call

--- TravelSchedule/App/ContentView.swift
+++ TravelSchedule/App/ContentView.swift
@@ -1,11 +1,5 @@
-//
-//  ContentView.swift
-//  TravelSchedule
-//
-//  Created by Anton Silenin on 16.01.2026.
-//
-
 import SwiftUI
+import OpenAPIURLSession
 
 struct ContentView: View {
     var body: some View {
@@ -16,6 +10,42 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+            testFetchStations()
+        }
+    }
+}
+
+
+func testFetchStations() {
+    
+    guard let apiKey = Bundle.main.infoDictionary?["YandexStationsAPIKey"] as? String else {
+        fatalError("API key is missing")
+    }
+    
+    Task {
+        do {
+            let client = Client(
+                serverURL: try Servers.Server1.url(),
+                transport: URLSessionTransport()
+            )
+            
+            let service = NearestStationsService(
+                client: client,
+                apikey: apiKey
+            )
+            
+            print("Fetching stations...")
+            let stations = try await service.getNearestStations(
+                lat: 59.864177,
+                lng: 30.319163,
+                distance: 50
+            )
+            
+            print("Successfully fetched stations: \(stations)")
+        } catch {
+            print("Error fetching stations: \(error)")
+        }
     }
 }
 
