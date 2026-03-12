a75c11575ad2ff4990d5a7b4ff306ee99ab4fa5c
Anton Silenin <anton-silenin@yandex.ru>
2026-03-12 14:39:09 +0300

    refactor: app cleanup

--- TravelSchedule/Core/Model/CitiesListState.swift
+++
@@ -1,6 +0,0 @@
-enum CitiesListState: Equatable {
-    case idle
-    case loading
-    case loaded
-    case failed(ErrorViewType)
-}
---
+++ TravelSchedule/Core/Types/TransferChoice.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum TransferChoice: String, CaseIterable, Identifiable, Sendable {
+    
+    case yes = "Да"
+    case no = "Нет"
+
+    var id: String { self.rawValue }
+}
